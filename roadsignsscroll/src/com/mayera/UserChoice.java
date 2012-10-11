393867f3922099ab0d6fa9ec62fabdf64cb14019
androidoptim <saradavelagapudi3@gmail.com>
2012-10-11 11:49:15 -0400

    Roadsigns Needs GUI changes.

---
+++ roadsignsscroll/src/com/mayera/UserChoice.java
@@ -0,0 +1,47 @@
+package com.mayera;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class UserChoice extends Activity {
+  Button mPractise, mTest;
+  Button mGoButton;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(com.mayera.R.layout.frontpage);
+    mPractise = (Button) findViewById(com.mayera.R.id.practise);
+    mTest = (Button) findViewById(com.mayera.R.id.test);
+
+    mPractise.setOnClickListener(new OnClickListener() {
+      public void onClick(View v) {
+        {
+          Intent myIntent2User = new Intent();
+          {
+            myIntent2User.setClassName("com.mayera",
+                "com.mayera.ScrollViewActivity");
+            startActivity(myIntent2User);
+          }
+        }
+      }
+    });
+
+    mTest.setOnClickListener(new OnClickListener() {
+      public void onClick(View v) {
+        {
+          Intent myIntent2User = new Intent();
+          {
+            myIntent2User.setClassName("com.mayera", "com.mayera.RoadSignQuiz");
+            startActivity(myIntent2User);
+          }
+        }
+      }
+    });
+
+  }
+}
